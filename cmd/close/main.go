f86964de22f88041c7badfac158329057df6993f
Cristian Chiru <cristi.chiru@gmail.com>
2025-06-25 23:29:32 +0300

    add log close demo

---
+++ cmd/close/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	log "github.com/thedataflows/go-lib-log"
+)
+
+func main() {
+	logger := log.NewLogger().Build()
+	defer logger.Close() // Ensures all messages are flushed
+
+	// Send many identical messages - these will be grouped
+	const MESSAGES = 100
+	fmt.Printf("Sending %d identical messages to demonstrate event grouping...\n", MESSAGES)
+	for range MESSAGES {
+		logger.Error().Msg("Database connection failed")
+	}
+
+	// Close() will flush the grouped message before exiting
+	// Without Close(), the grouped message might be lost
+}
